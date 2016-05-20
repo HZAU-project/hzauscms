da6b09894633f24a55ccd6578dd40abcce27b0a5
xbx <1060031655@qq.com>
2016-05-20 22:44:25 +0800

    login

---
+++ Application/Runtime/Cache/Home/50c9f5d2ef37c2cf131d5cf1381d3a4b.php
@@ -0,0 +1,44 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>HZAU学生综合管理系统</title>
+	<link rel="shortcut icon" href="/hzauscms/logo.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="/hzauscms/Public/front/css/login.css">
+</head>
+<body>
+<div id="wrapper">
+	<div class="header">
+		<div class="container"></div>
+	</div>
+	<div class="main">
+		<div class="container">
+			<div class="main-left">
+				<p>
+					
+				</p>
+			</div>
+			<div class="main-right">
+				<div class="form-head">登录</div>
+				<form action="">
+				
+				</form>	
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="container">
+			<span class="footer-left">
+				<h6>@2016 hzau</h6>
+				<a href="http://www.hzau.edu.cn/2014/ch/" target="blanket">华中农业大学</a>
+				<a href="http://coi.hzau.edu.cn/" target="blanket">信息学院</a>
+				<a href="http://jw.hzau.edu.cn/" target="blanket">教务系统</a>
+			</span>
+			<span class="footer-right">
+				技术支持　<a href="http://www.52feidian.com/" target="blanket"><img src="/hzauscms/Public/front/images/logo.png" alt="沸点"> 沸点工作室</a>
+			</span>
+		</div>
+	</div>
+</div>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hzauscms
+Huazhong Agricultural University Students Comprehensive Management System
+华中农业大学 学生综合管理系统
